3c0fbb97739b2ec27a69fb351e37efea462ae3db
Arun Kumar R <aruntutter19@gmail.com>
2023-07-24 15:11:27 +0530

    final code

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,40 @@
 "use strict";
 
 // global variables
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+let AMPM = document.getElementById("AMPM");
 
-// functions
-const init = () => {};
+// Function
+setInterval(() => {
+  let clock = new Date();
+  let hrs = clock.getHours();
+  let min = clock.getMinutes();
+  let sec = clock.getSeconds();
 
-//event listeners
+  if (hrs > 12) {
+    hrs = hrs - 12;
+  }
+  if (hrs < 10) {
+    hrs = "0" + hrs;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  if (hrs == 0) {
+    hrs = 12;
+  }
+  if (hrs < 12) {
+    AMPM.innerHTML = "PM";
+  } else {
+    AMPM.innerHTML = "AM";
+  }
 
-// initial settings
-init();
+  hours.innerHTML = hrs;
+  minutes.innerHTML = min;
+  seconds.innerHTML = sec;
+});
